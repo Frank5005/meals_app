75b9a1d8a4a21169db8c03cd394a419f097f6835
Frank5005 <frankill.5005@gmail.com>
2025-06-06 12:49:43 -0500

    Improving meals details and adding the tap bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 //import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/screens/categories.dart';
+//import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/tabs.dart';
 //import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
@@ -26,7 +27,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
     );
   }
 }
