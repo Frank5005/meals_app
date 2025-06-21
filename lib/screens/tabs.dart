3197379a7198e6abb6d6750addccd45a6a285e65
Frank5005 <frankill.5005@gmail.com>
2025-06-20 19:51:49 -0500

    Adding a little navigation

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -47,6 +47,15 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == 'filters') {
+      //
+    } else {
+      //You are in the same screen
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -66,7 +75,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +56,7 @@ class MainDrawer extends StatelessWidget {
                   ),
             ),
             onTap: () {
-              //
+              onSelectScreen('meals');
             },
           ),
           ListTile(
@@ -71,7 +73,7 @@ class MainDrawer extends StatelessWidget {
                   ),
             ),
             onTap: () {
-              //
+              onSelectScreen('filters');
             },
           ),
         ],
